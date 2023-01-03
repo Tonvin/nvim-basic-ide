51af556d58bf04078125ceb8d02e059dba9e9e70
Tonvin Tian <itonvin@gmail.com>
2023-01-03 05:35:47 +0000

    after indent do not stay in indent mode

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -57,8 +57,8 @@ keymap("i", "jk", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+-- keymap("v", "<", "<gv", opts)
+-- keymap("v", ">", ">gv", opts)
 
 -- Plugins --
 

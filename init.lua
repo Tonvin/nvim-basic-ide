c090c354f3650486e076c5b99643c40367e567fe
Tonvin Tian <itonvin@gmail.com>
2023-02-21 14:27:07 +0800

    github copilot select shortcuts set from tab to ctrl+j, because conflict with auto completement

--- init.lua
+++ init.lua
@@ -21,3 +21,5 @@ require "user.lsp"
 require "user.dap"
 
 vim.cmd[[ let g:go_def_mapping_enabled=0 ]]
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
